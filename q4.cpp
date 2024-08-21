9bfea5016ab31cf431889fe21fec4de842f6717a
SubhamAdhikary2000 <parimaladhikary360@gmail.com>
2024-08-21 17:02:51 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,25 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void bin(long n)
-{
-    long i;
-    cout << "0";
-    for (i = 1 << 31; i > 0; i = i / 2) {
-        if ((n & i) != 0) {
-            cout << "1";
-        }
-        else {
-            cout << "0";
-        }
+void display(int x){
+    int size;
+    size=32;
+    bool bits[size];
+    for(int i=0; i<size;++i){
+        bits[i]= false;
     }
+    for(int i=0;i<size;++i){
+        if(x&(1<<i)){
+                bits[i]=true;
+            }
+    }
+    
+    for(int i=size-1;i>0;--i){
+        if(bits[i])
+            cout<<1;
+        else 
+            cout<<0;
+    }
+    cout<< endl;
 }
 
-// Driver Code
-int main(void)
-{
-    bin(7);
-    cout << endl;
-    bin(4);
+int main(){
+    int x,y;
+    cout<<"Enter a positive and a negative integer"<<endl;
+    cin>>x>>y;
+    cout<<"Memory representation of the positive integer:"<<endl;
+    display(x);
+    cout<<"Memory representation of the Negative integer:"<<endl;
+    display(y);
 }
 
