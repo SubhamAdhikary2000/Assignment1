d3ac473716cbed6a30b0f77f9d7092ed74fbde8e
SubhamAdhikary2000 <parimaladhikary360@gmail.com>
2024-08-21 17:06:45 +0530

    Update q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long double x, rounded;
+    int k;
+    cout << "Enter the real number: ";
+    cin >> x;
+    cout << "Enter the positive integer k: ";
+    cin >> k;
+    rounded = x*k;
+    rounded = (long int) (rounded + 0.5f);
+    rounded /=k;
+    cout << "The rounded off number is: " << rounded << endl;
+    return 0;
+}
